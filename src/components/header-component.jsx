653a2f6c2e01386a7a2754c6d23fa92c44a51afa
mbrovarska <brovarskamariia@gmail.com>
2024-11-21 20:59:45 -0500

    tablet header finished

--- src/components/header-component.jsx
+++ src/components/header-component.jsx
@@ -4,16 +4,16 @@ import { Button } from "@material-tailwind/react"
 import arrowRight from "../assets/icon-arrow-right.svg"
 
 const Header = () => {
-    return <div className="grid grid-cols-2 lg:grid-cols-3">
-        <div className="bg-black hidden lg:block">
+    return <div className="grid grid-cols-2 lg:grid-cols-3 md:relative">
+        <div className="bg-black hidden lg:block lg:text-white">
             <p className="">{text.title}</p>
         </div>
         <div className="col-span-3 md:col-span-1 bg-bg">
             <img className="max-w-48 md:max-w-full block m-auto" src={headerImage} alt="man and woman look at a portrait of the old woman"/>
         </div>
-        <div className="col-span-3 md:col-span-1 px-3">
-            <p className="lg:hidden text-m font-bold">{text.title}</p> 
-            <p className="text-black text-small font-serif font-thin mb-4">{text.subtitle}</p>
+        <div className="col-span-3 md:col-span-1 md:absolute  md:top-24  lg:inset-y-1/2 md:left-[40%] lg:left-[60%] md:w-72 lg:w-80 px-3">
+            <p className="lg:hidden text-m font-bold md:font-medium">{text.title}</p> 
+            <p className="text-black text-small font-serif font-thin md:font-bold mb-4">{text.subtitle}</p>
             <Button size="lg"
                     color="black"
                     className="group relative flex items-center gap-3  hover:bg-orange overflow-hidden pr-[72px] rounded-none" type="button">
