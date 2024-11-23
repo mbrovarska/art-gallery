f156e773184e91d9c7c490313d0f619033fc6f3b
mbrovarska <brovarskamariia@gmail.com>
2024-11-22 20:09:44 -0500

    header finished

--- src/components/header-component.jsx
+++ src/components/header-component.jsx
@@ -2,23 +2,24 @@ import  headerImage from "../assets/desktop/image-hero.jpg"
 import text from "../assets/text/text.json"
 import { Button } from "@material-tailwind/react"
 import arrowRight from "../assets/icon-arrow-right.svg"
+import { styles } from "../styles/styles"
 
 const Header = () => {
-    return <div className="grid grid-cols-2 lg:grid-cols-3 md:relative">
-        <div className="bg-black hidden lg:block lg:text-white">
-            <p className="">{text.title}</p>
+    return <div className={styles.headerContainer}>
+        <div className={styles.headerColOne}>
+            <p className={styles.titleText}>{text.title}</p>
         </div>
-        <div className="col-span-3 md:col-span-1 bg-bg">
-            <img className="max-w-48 md:max-w-full block m-auto" src={headerImage} alt="man and woman look at a portrait of the old woman"/>
+        <div className={styles.headerColTwo}>
+            <img className={styles.headerImg} src={headerImage} alt="man and woman look at a portrait of the old woman"/>
         </div>
-        <div className="col-span-3 md:col-span-1 md:absolute  md:top-24  lg:inset-y-1/2 md:left-[40%] lg:left-[60%] md:w-72 lg:w-80 px-3">
-            <p className="lg:hidden text-m font-bold md:font-medium">{text.title}</p> 
-            <p className="text-black text-small font-serif font-thin md:font-bold mb-4">{text.subtitle}</p>
+        <div className={styles.headerColThree}>
+            <p className={styles.secondTitleText}>{text.title}</p> 
+            <p className={styles.subtitleText}>{text.subtitle}</p>
             <Button size="lg"
                     color="black"
-                    className="group relative flex items-center gap-3  hover:bg-orange overflow-hidden pr-[72px] rounded-none" type="button">
+                    className={styles.headerBtn} type="button">
                     {text.headerBtn}
-                <span className="absolute right-0 grid h-full w-12 place-items-center bg-orange transition-colors group-hover:bg-black">
+                <span className={styles.spanBtn}>
                     <img src={arrowRight}/>
                 </span>
             </Button>
---
+++ src/styles/styles.js
@@ -0,0 +1,12 @@
+export const styles= { 
+    headerContainer: "grid grid-cols-2 lg:grid-cols-3 md:relative",
+    headerColOne: "bg-black hidden lg:block lg:relative lg:text-white",
+    headerColTwo: "col-span-3 md:col-span-1 bg-bg",
+    headerColThree: "col-span-3 md:col-span-1 md:absolute md:top-24 lg:inset-y-1/2 md:left-[40%] lg:left-[60%] md:w-72 lg:w-80 px-3",
+    headerBtn: "group relative flex items-center gap-3  hover:bg-orange overflow-hidden pr-[72px] rounded-none",
+    spanBtn: "absolute right-0 grid h-full w-12 place-items-center bg-orange transition-colors group-hover:bg-black",
+    titleText: "absolute text-xl top-20 inset-3/4 min-w-fit",
+    headerImg: "max-w-48 md:min-w-full block m-auto",
+    secondTitleText: "lg:hidden text-m font-bold md:font-medium",
+    subtitleText: "text-black text-small font-serif font-thin md:font-bold mb-4"
+}
