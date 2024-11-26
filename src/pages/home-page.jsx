f8bdda2699bf30313a6e84c91f0596c27a5e157b
mbrovarska <brovarskamariia@gmail.com>
2024-11-26 16:24:09 -0500

    footer added

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/header-component";
 import Gallery from "../components/gallery-component";
+import Footer from "../components/footer-component";
 
 
 const HomePage = () => {
   return <>
     <Header/>
     <Gallery/>
+    <Footer/>
 
   </>
 }
