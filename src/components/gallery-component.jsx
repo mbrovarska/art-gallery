8fc2ec98f9af62654fdd7fa39f5f2ff57e8a9d6a
mbrovarska <brovarskamariia@gmail.com>
2024-11-25 19:06:56 -0500

    tablet content fixed

--- src/components/gallery-component.jsx
+++ src/components/gallery-component.jsx
@@ -4,7 +4,7 @@ import image3 from "../assets/mobile/image-grid-3@2x.jpg"
 import text from "../assets/text/text.json"
 
 const Gallery = () => {
-    return <div className="flex flex-col items-center mt-20 mx-2 md:grid md:grid-cols-6 md:relative">
+    return <div className="flex flex-col items-center mt-20 mx-2 md:ml-6 md:grid md:grid-cols-6 md:relative">
         <div className="w-[90%] md:col-start-3 md:col-end-7 md:order-2 md:w-[95%]">
             <img src={image1} alt="exibition of abstract paintings"/>
         </div>
@@ -15,10 +15,10 @@ const Gallery = () => {
         <div className="w-[90%] my-3 md:col-start-1 md:col-end-4 md:w-full md:order-3">
             <img src={image2} alt="exibition of abstract paintings"/>
         </div>
-        <div className="w-[90%] my-3 md:col-start-4 md:col-end-7 md:w-[95%] md:ml-2 md:mr-2 md:order-4 md:place-self-start">
+        <div className="w-[90%] my-3 md:col-start-4 md:col-end-7 md:w-[91%] md:ml-2 md:mr-12 md:order-4 md:place-self-start">
             <img src={image3} alt="exibition of abstract paintings"/>   
         </div>
-        <div className="w-[90%] bg-black p-2 md:w-[95%] md:col-start-4 md:col-end-7 md:ml-2 md:mr-2 md:order-4 md:absolute md:top-[68%] md:h-[30.8%]">
+        <div className="w-[90%] bg-black p-2 md:w-[91%] md:col-start-4 md:col-end-7 md:ml-2 md:order-4 md:absolute md:top-[67%] md:h-[32%]">
             <h2 className="text-s text-white font-bold">{text.subtitleContentTwo}</h2>
             <p className="text-small text-white font-serif font-thin">{text.contentTextTwo}</p>
         </div>
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -3,10 +3,10 @@ export const styles= {
     headerColOne: "bg-black hidden lg:block lg:relative lg:text-white",
     headerColTwo: "col-span-3 md:col-span-1 bg-bg",
     headerColThree: "col-span-3 md:col-span-1 md:absolute md:top-24 lg:inset-y-1/2 md:left-[40%] lg:left-[60%] md:w-72 lg:w-80 px-3",
-    headerBtn: "group relative flex items-center gap-3  hover:bg-orange overflow-hidden pr-[72px] rounded-none",
+    headerBtn: "group ml-4 md:ml-0 relative flex items-center gap-3  hover:bg-orange overflow-hidden pr-[72px] rounded-none",
     spanBtn: "absolute right-0 grid h-full w-12 place-items-center bg-orange transition-colors group-hover:bg-black",
     titleText: "absolute text-xl top-20 inset-3/4 min-w-fit",
     headerImg: "max-w-48 md:min-w-full block m-auto",
-    secondTitleText: "lg:hidden text-m font-bold md:font-medium",
-    subtitleText: "text-black text-small font-serif font-thin md:font-bold mb-4"
+    secondTitleText: "lg:hidden text-m ml-4 md:ml-0 font-bold md:font-medium",
+    subtitleText: "text-black text-small font-serif font-thin ml-4 md:ml-0 md:font-bold mb-4"
 }
